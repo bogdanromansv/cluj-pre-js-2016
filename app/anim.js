dc3da518d4be087b59fd1acf76e6bba8207cf02f
Bogdan Roman <bogdan.roman@softvision.ro>
2016-03-18 18:17:36 +0200

    Day5 work

--- app/anim.js
+++ app/anim.js
@@ -1,13 +1,57 @@
 
 function myFunction(){
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
+	document.forms['regform'].elements['regname'].focus();
 }
 
 
 
 
-function myFunction2() {
+function showPlaylist2() {
   var appear;
-  appear =document.getElementById("playlist");
+  appear =document.getElementById("playlist2");
   appear.style.display = 'block';
   }
+
+	function hidePlaylist2() {
+	  var appear;
+	  appear =document.getElementById("playlist2");
+	  appear.style.display = 'none';
+	  }
+
+		function showPlaylist1() {
+		  var appear;
+		  appear =document.getElementById("playlist1");
+		  appear.style.display = 'block';
+		  }
+
+			function hidePlaylist1() {
+			  var appear;
+			  appear =document.getElementById("playlist1");
+			  appear.style.display = 'none';
+			  }
+
+				function showPlaylist3() {
+					var appear;
+					appear =document.getElementById("playlist3");
+					appear.style.display = 'block';
+					}
+
+					function hidePlaylist3() {
+						var appear;
+						appear =document.getElementById("playlist3");
+						appear.style.display = 'none';
+						}
+
+	function validation(){
+
+			var email = document.getElementById("email").value;
+			var password = document.getElementById("password").value;
+
+
+			if ( email == "bogdan@gmail.com" && password == "bogdan"){
+						alert ("Login successfully");
+}
+			else
+			alert ("Login credentials incorrect")
+}
